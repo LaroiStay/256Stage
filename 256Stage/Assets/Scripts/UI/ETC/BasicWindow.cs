8206d7b2135b63053bc785861a9cb8acd5f1c02f
LaroiStay <91603746+LaroiStay@users.noreply.github.com>
2023-02-20 23:59:43 +0900

    아 나한테 왜그러는거야 정말로
    이건 계획에 없었잖아 진짜루
    아 어떻게 4시간동안한게 브랜치 한번 잘못바꿧다고 전부다 날아갈수가 있어 흑흑 진짜 너무 화가난다. 내가 경멸스럽다. 죽을거 같애 하 ㅋㅋㅋㅋㅋ 다시...

--- 256Stage/Assets/Scripts/UI/ETC/BasicWindow.cs
+++ 256Stage/Assets/Scripts/UI/ETC/BasicWindow.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicWindow : MonoBehaviour
+public class BasicWindow : UI_Scene
 {
-    // Start is called before the first frame update
-    void Start()
+    enum Panels
     {
-        
+        ColorPanel,
+        SelectObjectPanel
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }

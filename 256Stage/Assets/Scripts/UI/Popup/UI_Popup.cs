ba067df6afee031a79bb34f6f73514ac304dc658
LaroiStay <91603746+LaroiStay@users.noreply.github.com>
2023-02-23 03:04:51 +0900

    scene, base, popui

--- 256Stage/Assets/Scripts/UI/Popup/UI_Popup.cs
+++ 256Stage/Assets/Scripts/UI/Popup/UI_Popup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Popup : MonoBehaviour
+public class UI_Popup : UI_Base
 {
     public virtual void init()
     {
--- 256Stage/Assets/Scripts/UI/Scene/UI_Scene.cs
+++ 256Stage/Assets/Scripts/UI/Scene/UI_Scene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Scene : MonoBehaviour
+public class UI_Scene : UI_Base
 {
     public virtual void init()
     {

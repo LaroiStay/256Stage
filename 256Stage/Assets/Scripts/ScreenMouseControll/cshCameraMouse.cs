0daa408a2b58bddfc80283a04399550044d5e30e
LaroiStay <91603746+LaroiStay@users.noreply.github.com>
2023-03-03 14:10:39 +0900

    DragUpdate

--- 256Stage/Assets/Scripts/ScreenMouseControll/cshCameraMouse.cs
+++ 256Stage/Assets/Scripts/ScreenMouseControll/cshCameraMouse.cs
@@ -9,9 +9,9 @@ public class cshCameraMouse : MonoBehaviour
     [SerializeField] float m_zoomMax = 0f;
     [SerializeField] float m_zoomMin = 0f;
 
-    private float xRotate, yRotate, xRotateMove, yRotateMove;
+    float xRotate, yRotate, xRotateMove, yRotateMove;
     public float rotateSpeed = 500.0f;
-
+    float lastClickTime;
     void CameraZoom()
     {
         float t_zoomDirection = Input.GetAxis("Mouse ScrollWheel");
@@ -42,10 +42,64 @@ public class cshCameraMouse : MonoBehaviour
         }
     }
 
+
+
+    void DoubleClick()
+    {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Time.time - lastClickTime < 0.3f)
+            {
+                DoSomethingOnDoubleClick();
+            }
+            lastClickTime = Time.time;
+        }
+
+
+
+
+
+        
+    }
+
+
+    void DoSomethingOnDoubleClick()
+    {
+        if (CurrentObject.selectedCurrentObject != null)
+        {
+            Collider[] colliders = CurrentObject.selectedCurrentObject.GetComponentsInChildren<Collider>();
+            if (colliders.Length > 0)
+                for (int i = 0; i < colliders.Length; i++)
+                    colliders[i].enabled = true;
+            InMouse.IsOn = false;
+            CurrentObject.selectedCurrentObject = null;
+        }
+        else
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            GameObject go;
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                go = hit.collider.gameObject;
+                if (CurrentObject.selectedCurrentObject == null)
+                {
+                    InMouse.IsOn = true;
+                    CurrentObject.selectedCurrentObject = go;
+                }
+            }
+        }
+       
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         CameraZoom();
         CameraRotate();
+        DoubleClick();
     }
 }

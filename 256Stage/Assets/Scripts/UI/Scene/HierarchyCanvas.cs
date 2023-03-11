33608e7997aedbb30e69e7f1df425f28f82b22a0
LaroiStay <91603746+LaroiStay@users.noreply.github.com>
2023-03-11 16:10:23 +0900

    하이라키 업데이트

--- 256Stage/Assets/Scripts/UI/Scene/HierarchyCanvas.cs
+++ 256Stage/Assets/Scripts/UI/Scene/HierarchyCanvas.cs
@@ -17,15 +17,25 @@ public class HierarchyCanvas : UI_Scene
         Text
     }
 
+    enum Buttons
+    {
+        On,
+        Close
+    }
+
+
     private void Start()
     {
         BindThings();
         SetText();
+        BindFucToButtons();
+        FirstSetting();
     }
 
     void BindThings()
     {
         Bind<Image>(typeof(Panels));
+        Bind<Button>(typeof(Buttons));
         Bind<TextMeshProUGUI>(typeof(Texts));
     }
 
@@ -41,5 +51,35 @@ public class HierarchyCanvas : UI_Scene
         HiButton.setGameObject(go);
     }
 
+
+   void FirstSetting()
+    {
+        Get<Button>((int)Buttons.On).gameObject.SetActive(false);
+    }
+
+    void BindFucToButtons()
+    {
+        Get<Button>((int)Buttons.Close).onClick.AddListener(Close);
+        Get<Button>((int)Buttons.On).onClick.AddListener(On);
+
+    }
+
+    void Close()
+    {
+        Debug.Log("good");
+        Get<Image>((int)Panels.HierarchyPanel).gameObject.SetActive(false);
+        Get<TextMeshProUGUI>((int)Texts.Text).gameObject.SetActive(false);
+        Get<Button>((int)Buttons.On).gameObject.SetActive(true);
+        Get<Button>((int)Buttons.Close).gameObject.SetActive(false);
+
+
+    }
+    void On()
+    {
+        Get<Image>((int)Panels.HierarchyPanel).gameObject.SetActive(true);
+        Get<TextMeshProUGUI>((int)Texts.Text).gameObject.SetActive(true);
+        Get<Button>((int)Buttons.On).gameObject.SetActive(false);
+        Get<Button>((int)Buttons.Close).gameObject.SetActive(true);
+    }
 
 }

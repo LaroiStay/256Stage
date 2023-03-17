5e250785c813985f7ce18a74e1cfc78512534eb9
LaroiStay <91603746+LaroiStay@users.noreply.github.com>
2023-03-17 16:06:47 +0900

    순간이동
    성공했당 히히

--- 256Stage/Assets/Scripts/UI/ETC/Draggable.cs
+++ 256Stage/Assets/Scripts/UI/ETC/Draggable.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler
 {
     RectTransform rectTransform;
+    GameObject LightObject;
     Vector2 offset;
+    Vector3 LightOffset;
     string temp_name;
     int temp_key;
     bool isFirst = true;
+    bool FirstMakeObject = true;
+    bool isLight = false;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+      
+    }
+
+
+    private void Start()
+    {
+        IsLight Li = GetComponent<IsLight>();
+        if (Li == null)
+            isLight = false;
+        else
+            isLight = true;
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -23,30 +39,93 @@ public class Draggable : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndD
 
     public void OnDrag(PointerEventData eventData)
     {
-        if (isFirst)
+
+        if(isLight == false)
         {
-            isFirst = false;   
-            temp_key = GetComponent<RealSelectButton>().m_key;
-            temp_name = GetComponent<RealSelectButton>().m_name;
-            GameObject go = Manager.Resource_Instance.Instantiate("UI/ETC/RealSelectButton", this.transform.parent);
-            go.transform.SetSiblingIndex(temp_key - 1);
-            go.GetComponent<RealSelectButton>().SetImage(temp_name, temp_key);
+            if (isFirst)
+            {
+                isFirst = false;
+                temp_key = GetComponent<RealSelectButton>().m_key;
+                temp_name = GetComponent<RealSelectButton>().m_name;
+                GameObject go = Manager.Resource_Instance.Instantiate("UI/ETC/RealSelectButton", this.transform.parent);
+                go.transform.SetSiblingIndex(temp_key - 1);
+                go.GetComponent<RealSelectButton>().SetImage(temp_name, temp_key);
+            }
+            rectTransform.position = eventData.position - offset;
         }
-        rectTransform.position = eventData.position - offset;
+        else
+        {
+            if (isFirst)
+            {
+                isFirst = false;
+                temp_key = GetComponent<RealSelectButton>().m_key;
+                temp_name = GetComponent<RealSelectButton>().m_name;
+                GameObject go = Manager.Resource_Instance.Instantiate("UI/ETC/RealSelectButton", this.transform.parent);
+                go.transform.SetSiblingIndex(temp_key - 1);
+                go.GetComponent<RealSelectButton>().SetImage(temp_name, temp_key);
+                this.GetComponent<RealSelectButton>().GetComponentInChildren<Button>().gameObject.SetActive(false);
+
+
+
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10000.0f))
+            {
+
+
+                if (TurssQueue.GameObjects.Count != 0)
+                {
+                    if (FirstMakeObject)
+                    {
+                        LightObject = Manager.Resource_Instance.Instantiate($"Stage/{temp_name}/{temp_name}{temp_key}");
+
+                    }
+                    GameObject[] tempList = TurssQueue.GameObjects.ToArray();
+                    float distance = 1000.0f;
+
+                    for (int i = 0; i < TurssQueue.GameObjects.Count; i++)
+                    {
+                        if (distance > (hit.point - tempList[i].transform.position).magnitude)
+                        {
+                            distance = (hit.point - tempList[i].transform.position).magnitude;
+                            LightOffset = tempList[i].transform.position;
+                        }
+                    }
+
+                    Debug.Log(LightOffset);
+
+                    LightObject.transform.position = LightOffset;
+                    if (FirstMakeObject)
+                    {
+                        FirstMakeObject = false;
+                        FindObjectOfType<HierarchyCanvas>().PlusPrefabsInHierarchy(temp_name, temp_key, LightObject);
+                    }
+
+                }
+
+            }
+
+        }
+       
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 1000.0f)){
-            GameObject go = Manager.Resource_Instance.Instantiate($"Stage/{temp_name}/{temp_name}{temp_key}");
-            go.transform.position = hit.point;
-            FindObjectOfType<HierarchyCanvas>().PlusPrefabsInHierarchy(temp_name, temp_key,go);
+        if (!isLight)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000.0f))
+            {
+                GameObject go = Manager.Resource_Instance.Instantiate($"Stage/{temp_name}/{temp_name}{temp_key}");
+                go.transform.position = hit.point;
+                FindObjectOfType<HierarchyCanvas>().PlusPrefabsInHierarchy(temp_name, temp_key, go);
 
+            }
+
+            Manager.Resource_Instance.Destroy(this.gameObject);
         }
-
-        Manager.Resource_Instance.Destroy(this.gameObject);
     }
 }

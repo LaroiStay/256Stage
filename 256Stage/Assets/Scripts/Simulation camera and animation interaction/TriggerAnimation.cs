326a7ebd339aad18de986f72a280006afbade233
yoonyoon66 <oldlotus55@gmail.com>
2023-04-20 00:34:40 +0900

    카메라 상호작용

---
+++ 256Stage/Assets/Scripts/Simulation camera and animation interaction/TriggerAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false; // 애니메이션 비활성화
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // 카메라와 충돌체 감지
+        {
+            animator.enabled = true; // 애니메이션 활성화
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // 카메라와 충돌체 감지
+        {
+            animator.enabled = false; // 애니메이션 비활성화
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
